ab0b0f77f4f915575fd5253b9433334f5be8b4ee
Arnav-03 <arnavarora0003@gmail.com>
2024-07-26 00:35:19 +0530

    mkc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,6 +61,11 @@ export default function Home() {
           </div>
         </div>
 
+        <ProductReel
+          query={{ sort: 'desc', limit: 4 }}
+          href='/products?sort=recent'
+          title='Brand new'
+        />
       </MaxWidthWrapper>
 
       <section className='border-t border-gray-200 bg-gray-50'>
